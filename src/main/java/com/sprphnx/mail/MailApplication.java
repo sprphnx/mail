32395ab1acc8cb4accdbe96c2f9d5d41800e65e4
sprphnx <sprphnx@outlook.com>
2019-09-17 19:44:53 +0300

    Initial flow completed.

--- src/main/java/com/sprphnx/mail/MailApplication.java
+++ src/main/java/com/sprphnx/mail/MailApplication.java
@@ -1,13 +1,32 @@
 package com.sprphnx.mail;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
-public class MailApplication {
+public class MailApplication implements CommandLineRunner {
 
+	@Autowired
+    private JavaMailSender javaMailSender;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MailApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("xyz@gmail.com");
+
+        msg.setSubject("Mail from SprPhnX");
+        msg.setText("This is a mail sent. Special thanks to MKYONG.com for this example :) ");
+
+        javaMailSender.send(msg);
+		
 	}
 
 }
